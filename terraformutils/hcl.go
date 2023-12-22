2601f65ffef6e0a41a781a3fdf26dc51b567fc5d
Alex Mingoia <alex@alexmingoia.com>
2023-12-23 01:43:07 +0630

    Don't prefix resource names with `tfer--`

--- terraformutils/hcl.go
+++ terraformutils/hcl.go
@@ -277,7 +277,6 @@ func escapeRune(s string) string {
 // Sanitize name for terraform style
 func TfSanitize(name string) string {
 	name = unsafeChars.ReplaceAllStringFunc(name, escapeRune)
-	name = "tfer--" + name
 	return name
 }
 
